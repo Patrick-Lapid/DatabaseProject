89f5bdae704db4b5a3df281510d920c1d55672a3
Xian Jian Zhang <xianjianzhang.yahoo@gmail.com>
2022-03-28 15:11:55 -0400

    Fixed up models, data uploaded into oracle

--- api/serializers.py
+++ api/serializers.py
@@ -1,7 +1,12 @@
 from rest_framework import serializers
-from .models import Gun
+from .models import Gun, Crime
 
 class GunSerializer(serializers.ModelSerializer):
     class Meta:
         model = Gun
         fields = ('id', 'gunID', 'crimeID', 'stolen')
+
+class CrimeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Crime
+        fields = ('crimeID', 'date', 'state', 'address', 'sourceURL', 'latitude', 'longitude')
--- api/urls.py
+++ api/urls.py
@@ -3,6 +3,7 @@ from .views import *
 
 urlpatterns = [
     path('', GunCreateView.as_view()),
+    path('crime/', CrimeCreateView.as_view()),
     path('list/', GunListView.as_view()),
     path('delete/<int:pk>/', GunDeleteView.as_view()),
 ]
