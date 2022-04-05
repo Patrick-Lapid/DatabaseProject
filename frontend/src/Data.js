a35f7df3095bbebd7d13ed3097928b71a4a319b7
Patrick-Lapid <patrickklapid@gmail.com>
2022-04-05 13:01:03 -0400

    Added Mock Charts

---
+++ frontend/src/Data.js
@@ -0,0 +1,76 @@
+export const QueryOneBar = [
+    {
+      id: 1,
+      age: '0-14 years old',
+      dead: .05,
+      unharmed: .75,
+      arrested: .2,
+    },
+    {
+      id: 2,
+      age: '15-25 years old',
+      dead: .15,
+      unharmed: .73,
+      arrested: .85,
+    },
+    {
+      id: 3,
+      age: '26-64 years old',
+      dead: .36,
+      unharmed: .5,
+      arrested: .8,
+    },
+    {
+      id: 4,
+      age: '65+ years old',
+      dead: .02,
+      unharmed: .90,
+      arrested: .9,
+    },
+  ];
+  
+  export const QueryOneSuspect = [
+    {
+      id: 1,
+      age: '0-14 years old',
+      value: 4
+    },
+    {
+      id: 2,
+      age: '15-25 years old',
+      value: 24,
+    },
+    {
+      id: 3,
+      age: '26-64 years old',
+      value: 60,
+    },
+    {
+      id: 4,
+      age: '65+ years old',
+      value: 2,
+    },
+  ];
+  
+  export const QueryOneVictim = [
+    {
+      id: 1,
+      age: '0-14 years old',
+      value: 12
+    },
+    {
+      id: 2,
+      age: '15-25 years old',
+      value: 37,
+    },
+    {
+      id: 3,
+      age: '26-64 years old',
+      value: 94,
+    },
+    {
+      id: 4,
+      age: '65+ years old',
+      value: 18,
+    },
+  ];
