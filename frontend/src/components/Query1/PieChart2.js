6b8d47d2b88afb740748d6ae88730bd4dd086aa6
Vincent <flemingvincent@ufl.edu>
2022-04-07 13:48:50 -0400

    updated file structure & added mock charts for queries 2, 4, and 5.

--- frontend/src/components/Query1/PieChart2.js
+++ frontend/src/components/Query1/PieChart2.js
@@ -2,8 +2,8 @@ import React from "react";
 import { Pie } from "react-chartjs-2";
 import { Chart as ChartJS } from "chart.js/auto";
 
-function PieChart({ chartData }) {
+function PieChart2({ chartData }) {
   return <Pie data={chartData} />;
 }
 
-export default PieChart;
+export default PieChart2;
