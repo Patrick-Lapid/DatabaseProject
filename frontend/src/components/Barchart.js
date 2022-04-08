6cc8b37c9a1714731cc78d17f4ddac1d30096fec
skylarmccain <skylarmccain@ufl.edu>
2022-04-08 04:07:29 -0400

    Update Barchart.js

--- frontend/src/components/Barchart.js
+++ frontend/src/components/Barchart.js
@@ -1,8 +1,9 @@
-import React from "react";
-import { Bar } from 'react-chartjs-2';
+import React form 'react';
+import {} from 'react-chartjs-2';
+import {Chart as ChartJS} from 'chart.js/auto'
 
-function BarChart({ chartData }) {
-  return <Bar data={chartData} />;
+function BarChart(chartData) {
+    return <Bar data={chartData} options={}/>;
 }
 
 export default BarChart;
