6f2602e2973394634c7595e94313e1ca1b5838eb
skylarmccain <skylarmccain@ufl.edu>
2022-04-08 04:07:45 -0400

    idk

--- frontend/src/components/Barchart.js
+++ frontend/src/components/Barchart.js
@@ -1,9 +1,8 @@
-import React form 'react';
-import {} from 'react-chartjs-2';
-import {Chart as ChartJS} from 'chart.js/auto'
+import React from "react";
+import { Bar } from 'react-chartjs-2';
 
-function BarChart(chartData) {
-    return <Bar data={chartData} options={}/>;
+function BarChart({ chartData }) {
+  return <Bar data={chartData} />;
 }
 
 export default BarChart;
